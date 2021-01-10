0987ea73390d58802937d7303ccf335357ba01f6
migben <itsmigben@gmail.com>
2021-01-10 12:10:47 -0500

    went over Agile and Scrum

---
+++ agileScrum/index.js
@@ -0,0 +1,26 @@
+/**
+* Front End Interview Questions: Agile & Scrum
+* 
+* Q: What is Agile? What is Scrum?
+*
+* Bonus Resource: https://scrumguides.org/
+*
+Agile is really just a process how we run our engineering teams at
+the end of the day. Is about setting some processes and procedures
+
+Typically the most common version of this is Scrum. This is more centered
+about how many points have we accumulated in our tickets. Our goal is to have
+an actionable amount of work that we can learn from each sprint.
+
+Sprint is typically a 2 week period of time that we deliver some form of value.
+In our sprints, we could have stand ups in the morning where we talk what are we doing
+what is blocking us and what we did the day before.
+
+Agile and Scrum is all about team agreement. Pulling things in and out, when the team 
+says this makes sense.
+
+When does teams agreen that things are done /  finish. (this could vary from
+    organization to organization.) Usually we could have a retrospective at the end
+    or at the start of the next sprint. This is mostly to reflect how the team did and
+    any potential changes we could make.
+*/
