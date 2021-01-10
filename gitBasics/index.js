8c3600407db0005291b30586e6d25b4e3cc9055d
migben <itsmigben@gmail.com>
2021-01-10 11:35:58 -0500

    went over what is git and the basic commands

---
+++ gitBasics/index.js
@@ -0,0 +1,18 @@
+/**
+ * Front End Interview Questions: Git Fundamentals
+ * 
+ * Q: What is Git? Why do you use it? What are some common commands you use?
+ * Bonus Resource: https://try.github.io/
+ * 
+ * https://education.github.com/git-cheat-sheet-education.pdf
+ * 
+ * is the most popular tool for version control.
+ * 
+ * popular commands
+ * branch
+ * stage / add
+ * commit -m
+ * pull -> the code down from the repo
+ * push -> pushing back up to the repo or just updating the repo
+ */
+
