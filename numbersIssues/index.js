a27f8c9658922f08d4c98babf21812626b911b03
migben <itsmigben@gmail.com>
2021-01-10 15:51:47 -0500

    went over issues with numbers in JS

---
+++ numbersIssues/index.js
@@ -0,0 +1,14 @@
+/**
+ * Front End Interview Questions: Number Issues
+ * 
+ * Q: What are some issues with numbers in JavaScript you 
+ * may have to protect against?
+ * 
+ * The may problem with numbers will arise when we are dealing
+ * with applications that work with money.
+ * 
+ * As seen below the decimal value is a little bit off.
+ * For this type of situation with should always round it.
+ */
+
+console.log(0.2 + 0.1); // 0.30000000000000004
