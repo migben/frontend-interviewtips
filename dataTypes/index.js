f114ffd1a492fc2471d7a9ceb218a57fa197b20c
migben <itsmigben@gmail.com>
2021-01-10 10:10:40 -0500

    went over the basics of datatypes

---
+++ dataTypes/index.js
@@ -0,0 +1,14 @@
+/**
+ * Front End Interview Questions: Data Types
+ * 
+ * Q: What are some JavaScript data types?
+ * 
+ * First, one of the reason why JS is a great beginner programming language.
+ * It's the simplicity compared to other languages.
+ * 
+ * When it comes to data types we have primitives and non-primitives.
+ * 
+ * Primitives: boolean, strings, numbers, BigInt and Symbol
+ * Non-primitives: arrays [], Objects {}, Map, Set
+ */
+
